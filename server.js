73f4e7f48f180676137624dd544e847c79409bb9
Tim Joosten <tim_joosten@hotmail.com>
2018-10-08 14:34:57 +0200

    deploy live

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/waterquality-frontend'));
+app.use(express.static('./dist/waterquality-frontend'));
 
-app.get('*', (req, res) => {
-  res.sendFile(`./waterquality-frontend/dist/index.html`);
+app.get('/*', function(req,res) {
+  res.sendFile(path.join(__dirname,'/dist/waterquality-frontend/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
