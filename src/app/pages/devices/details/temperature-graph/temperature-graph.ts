e42b27c3b3d1e0a5d9cb6b1246bf4faa3a73e165
t-joosten <tim_joosten@hotmail.com>
2018-11-14 21:11:33 +0100

    create test data

--- src/app/pages/devices/details/temperature-graph/temperature-graph.ts
+++ src/app/pages/devices/details/temperature-graph/temperature-graph.ts
@@ -1,5 +1,6 @@
 import {Component, AfterViewInit, OnDestroy, Input} from '@angular/core';
 import {NbThemeService} from '@nebular/theme';
+import {now} from "moment";
 
 @Component({
   selector: 'ngx-temperature-graph',
@@ -21,92 +22,78 @@ export class TemperatureGraphComponent implements AfterViewInit, OnDestroy {
 
       let temperatures = [];
       let dates = [];
+      let dataFormatted = []
 
       for (let i = 0; i < this.data.length - 1; i++) {
         let date = new Date(this.data[i].createdAt);
         temperatures.push(this.data[i].values['Temperature']);
         dates.push(date);
+
+        let name = date.toString();
+        let value = [
+          [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/'),
+          this.data[i].values['Temperature']
+        ];
+
+        console.log(`name: ${name}`);
+        console.log(`value: ${value}`);
+
+        dataFormatted.push({
+          name: name,
+          value: [
+            [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('/'),
+            this.data[i].values['Temperature']
+          ]
+        });
       }
 
-      console.log(temperatures);
-
-      const colors: any = config.variables;
-      const echarts: any = config.variables.echarts;
+      var now = +new Date(1997, 9, 3);
+      var oneDay = 24 * 3600 * 1000;
+      var value = Math.random() * 1000;
 
       this.options = {
-        backgroundColor: echarts.bg,
-        color: [colors.danger, colors.primary, colors.info],
+        title: {
+          text: 'Temperatuur'
+        },
         tooltip: {
-          trigger: 'item',
-          formatter: '{a} <br/>{b} : {c}',
+          trigger: 'axis',
+          formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+          },
+          axisPointer: {
+            animation: false
+          }
         },
-        legend: {
-          left: 'left',
-          data: ['Temperatuur'],
-          textStyle: {
-            color: echarts.textColor,
-          },
+        xAxis: {
+          type: 'time',
+          splitLine: {
+            show: false
+          }
         },
-        xAxis: [
-          {
-            type: 'time',
-            data: dates,
-            minInterval: 5,
-            axisTick: {
-              alignWithLabel: true,
-            },
-            axisLine: {
-              lineStyle: {
-                color: echarts.axisLineColor,
-              },
-            },
-            axisLabel: {
-              textStyle: {
-                color: echarts.textColor,
-              },
-            },
-          },
-        ],
-        yAxis: [
-          {
-            type: 'value',
-            min: 0,
-            max: 100,
-            axisLine: {
-              lineStyle: {
-                color: echarts.axisLineColor,
-              },
-            },
-            splitLine: {
-              lineStyle: {
-                color: echarts.splitLineColor,
-              },
-            },
-            axisLabel: {
-              textStyle: {
-                color: echarts.textColor,
-              },
-            },
-          },
-        ],
-        grid: {
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          containLabel: true,
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%'],
+          splitLine: {
+            show: false
+          }
         },
-        series: [
-          {
-            name: 'Temperatuur',
-            type: 'line',
-            data: temperatures,
-          },
-        ],
+        series: [{
+          name: '模拟数据',
+          type: 'line',
+          showSymbol: false,
+          hoverAnimation: false,
+          data: dataFormatted
+        }]
       };
+
     });
   }
 
   ngOnDestroy(): void {
     this.themeSubscription.unsubscribe();
   }
+
+  
 }
